b595b4b68deacedc0c8f17ddfe52ec28e1e08741
pallavi <pallaviraj5992@gmail.com>
2020-09-19 19:34:16 +0530

    First commit

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('country','App\Http\Controllers\Country\CountryController@country');
+Route::get('country/{id}','App\Http\Controllers\Country\CountryController@countryById');
+Route::post('country','App\Http\Controllers\Country\CountryController@countrysave');
+Route::put('country/{country}','App\Http\Controllers\Country\CountryController@countryUpdate');
+Route::delete('country/{country}','App\Http\Controllers\Country\CountryController@countryDelete');
